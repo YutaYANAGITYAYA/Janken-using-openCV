f77bd93c7117caeeb151d1976be96829d2b1ad2c
YutaYANAGITYAYA <argentum.sechthdovipopraran@gmail.com>
2017-05-28 21:07:55 +0900

    Update janken.py

--- janken.py
+++ janken.py
@@ -8,21 +8,24 @@ def show_result(x,y):
     print (u"　　あなたの手: %s"%hand[y])
     res = resMat[x][y]
     print (result[res])
-    img = cv2.imread(os.path.join("data","%s.jpg"%res))
-    cv2.imshow("janken",img)
+    sumres[0 if res==1 else 1 if res==-1 else 2] += 1
+    print (u"{0[0]}勝 {0[1]}敗\n".format(sumres))
+    im = cv2.imread(os.path.join("data","%s.jpg"%res))
+    cv2.imshow("janken", im)
 
 def counter():
-    # 取得した画像から肌色抽出
     ret, im = cam.read()
-    im = cv2.bilateralFilter(im, 7, 21, 21)
-    im = cv2.bilateralFilter(im, 7, 21, 21)
+    im = cv2.bilateralFilter(im, 5, 16, 16)
+    im = cv2.bilateralFilter(im, 5, 16, 16)
     hsv = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-    hsv = cv2.cvtColor(hsv, cv2.COLOR_RGB2HSV) 
-    mask = cv2.inRange(hsv, np.array([5,31,31]), np.array([15,255,255]))
+    hsv = cv2.cvtColor(hsv, cv2.COLOR_RGB2HSV)
+    H = cv2.getTrackbarPos('Hue', 'CamImg')
+    minH = max(0, H-5) if H<175 else 170
+    maxH = min(180, H+5) if H>5 else 10
+    mask = cv2.inRange(hsv, np.array([minH,31,31]), np.array([maxH,255,255]))
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
-    # 最大輪郭のものに対して指の数をカウント
     contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[1]
     count = 0
     if len(contours)>0:
@@ -37,46 +40,49 @@ def counter():
             end = tuple(cnt[e_idx][0])
             far = tuple(cnt[f_idx][0])
             dst = np.sqrt((start[0]-end[0])**2+(start[1]-end[1])**2)
-            if 25<dst<150 and 20000<depth<50000:
+            if 25<dst<175 and 20000<depth<50000:
                 count += 1
                 cv2.line(im,start,far,(0,255,0))
                 cv2.line(im,far,end,(0,255,0))
                 cv2.circle(im,far,3,(0,0,255),-1)
     
-    cv2.imshow("CamImg",im)
+    cv2.imshow("CamImg", im)
     cv2.waitKey(1)
     return count
 
+def nothing(x):
+    pass
 
 hand = {0:u"グー", 1:u"チョキ", 2:u"パー"}
-result = {-1:u"チノちゃんの勝ち\n", 0:u"あいこ．．．\n", 1:u"あなたの勝ち\n"}
-resMat=[[ 0,-1, 1],
-        [ 1, 0,-1],
-        [-1, 1, 0]]
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(7,7))
+result = {-1:u"チノちゃんの勝ち", 0:u"あいこ．．．", 1:u"あなたの勝ち"}
+resMat = [[0,-1,1],[1,0,-1],[-1,1,0]]
+sumres = [0,0,0]
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15,15))
 cam = cv2.VideoCapture(0)
 mov = cv2.VideoCapture(os.path.join("data","chino.avi"))
 
-while (cv2.waitKey(2000)!=27):
-    mov.set(cv2.CAP_PROP_POS_FRAMES,0)
+cv2.namedWindow('CamImg')
+cv2.createTrackbar('Hue', 'CamImg', 10, 180, nothing)
+
+while cv2.waitKey(2000) != 27:
+    mov.set(cv2.CAP_PROP_POS_FRAMES, 0)
     while (1):
-        ret, img = mov.read()
+        ret, im = mov.read()
         if not ret:
             break
-        cv2.imshow("janken",img)
+        im = cv2.resize(im, (854,480))
+        cv2.imshow("janken", im)
         counter()
-        cv2.waitKey(33)
+        cv2.waitKey(10)
     
-    fing = 0
-    for i in range(10):
-        fing += counter()
+    fing = sum([counter() for _ in range(10)])
     yy = 0 if fing<5 else 1 if fing<25 else 2
     cc = np.random.randint(3)
-    img = cv2.imread(os.path.join("data","%s.png"%cc))
-    cv2.imshow("janken",img)
+    im = cv2.imread(os.path.join("data","%s.png"%cc))
+    im = cv2.resize(im, (854,480))
+    cv2.imshow("janken", im)
     cv2.waitKey(1000)
-    show_result(cc,yy)
-
+    show_result(cc, yy)
 
 cam.release()
 cv2.destroyAllWindows()
